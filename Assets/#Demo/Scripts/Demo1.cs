0f5b44e0ba41eb1d31c83cff82a5228150b3adf7
Gigacee <gigacee@gigacreation.jp>
2021-09-18 14:47:12 +0900

    プロジェクトを追加

---
+++ Assets/#Demo/Scripts/Demo1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+#if !UNITY_EDITOR && UNITY_WEBGL
+using System.Runtime.InteropServices;
+#endif
+
+public class Demo1 : MonoBehaviour
+{
+#if !UNITY_EDITOR && UNITY_WEBGL
+    [DllImport("__Internal")]
+    private static extern string TweetFromUnity(string rawMessage);
+#endif
+
+    public void Tweet()
+    {
+#if !UNITY_EDITOR && UNITY_WEBGL
+        TweetFromUnity("Tweet Message");
+#endif
+    }
+}
---
+++ Assets/#Demo/Scripts/Demo2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Xml.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+#if !UNITY_EDITOR && UNITY_WEBGL
+using System.Runtime.InteropServices;
+#endif
+
+public class Demo2 : MonoBehaviour
+{
+    [SerializeField] private string _imgurClientId;
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+    [DllImport("__Internal")]
+    private static extern string TweetFromUnity(string rawMessage);
+#endif
+
+    public void TweetWithScreenshot()
+    {
+        StartCoroutine(TweetWithScreenshotCo());
+    }
+
+    private IEnumerator TweetWithScreenshotCo()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
+
+        var wwwForm = new WWWForm();
+        wwwForm.AddField("image", Convert.ToBase64String(tex.EncodeToJPG()));
+        wwwForm.AddField("type", "base64");
+
+        // Upload to Imgur
+        UnityWebRequest www = UnityWebRequest.Post("https://api.imgur.com/3/image.xml", wwwForm);
+        www.SetRequestHeader("AUTHORIZATION", "Client-ID " + _imgurClientId);
+
+        yield return www.SendWebRequest();
+
+        var uri = "";
+
+        if (!www.isNetworkError)
+        {
+            XDocument xDoc = XDocument.Parse(www.downloadHandler.text);
+            uri = xDoc.Element("data")?.Element("link")?.Value;
+
+            // Remove Ext
+            uri = uri?.Remove(uri.Length - 4, 4);
+        }
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+        TweetFromUnity($"Tweet Message%0a{uri}");
+#endif
+    }
+}
